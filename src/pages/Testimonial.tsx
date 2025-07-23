f8283eed813f4e3191717e0a1fb07da7b9035f9d
B FAROOQ <bfarooq2022@gmail.com>
2025-07-23 14:40:37 +0530

    responsiveness added

--- src/pages/Testimonial.tsx
+++ src/pages/Testimonial.tsx
@@ -1,19 +1,26 @@
-
-
 export const Testimonial = () => {
-
-    return (
-        <div className="mt-10 py-3">
-            <div className="flex justify-around items-center gap-4 ml-10">
-                <div className="shadow-sm flex flex-col justify-around items-center p-8 max-w-7xl">
-                    <p className="py-4 w-1/2 text-center items-center text-xl font-serif">" TechSpark boosted our productivity by 50%! we can't imagine working without it. "</p>
-                    <p className="text-end items-center ml-42 text-gray-700">Name, CTO @ TechSpark</p>
-                </div>
-                <div className="shadow-sm flex flex-col justify-around items-center p-8 max-w-7xl">
-                    <p className="py-4 w-1/2 text-center items-center text-xl font-serif">" A game changer for fast-moving teams. Simple, Powerful, and elegant. "</p>
-                    <p className="text-end items-center ml-42 text-gray-700">Name, PM @ TechSpark</p>
-                </div>
-            </div>
+  return (
+    <div className="mt-10 py-3 p-4 md:py-4 md:p-1">
+      <div className="flex flex-col justify-center items-center gap-4 m-6 md:flex md:flex-row">
+        <div className="shadow-sm flex flex-col justify-around items-center p-8 max-w-7xl">
+          <p className="py-4 w-fit md:w-1/2 text-center items-center text-2xl md:text-xl font-serif">
+            " TechSpark boosted our productivity by 50%! we can't imagine
+            working without it. "
+          </p>
+          <p className="text-end text-xs md:text-md items-center ml-42 text-gray-700">
+            Name, CTO @ TechSpark
+          </p>
         </div>
-    )
-}
+        <div className="shadow-sm flex flex-col justify-around items-center p-8 max-w-7xl">
+          <p className="py-4 w-fit md:w-1/2 text-center items-center text-2xl md:text-xl font-serif">
+            " A game changer for fast-moving teams. Simple, Powerful, and
+            elegant. "
+          </p>
+          <p className="text-end text-xs md:text-md items-center ml-42 text-gray-700">
+            Name, PM @ TechSpark
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
